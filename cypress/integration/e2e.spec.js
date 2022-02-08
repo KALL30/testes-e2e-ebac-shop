d730d313cf78581432b4a534bf9231e1d7263224
Kalleo Sales <kalleosales12@gmail.com>
2022-02-08 12:20:57 -0300

    Modificação teste

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -14,7 +14,7 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+        kakakak
     });
 
 
