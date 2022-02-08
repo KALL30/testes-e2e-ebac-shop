257b2d396a09e170bbccc7bb80774fc6cf576f66
Kalleo Sales <kalleosales12@gmail.com>
2022-02-08 17:45:34 -0300

    Modificação Repositorios 2.1

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -14,7 +14,7 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        Modificação repositorio 2.0
+        Modificação repositorio 2.1
     });
 
 
